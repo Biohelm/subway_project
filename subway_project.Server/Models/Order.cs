be28f90a4ed8574e6df7a1641792f139f3126f38
RusandeKodare <o.p.j@hotmail.com>
2025-04-15 13:07:34 +0200

    Refactor Order class properties and initialization
    - Changed initialization of `Products` from `new()` to `[]`.
    - Added new properties: `Subs` and `Toppings`, both initialized to `[]`.
    - Introduced `QueueId` property of type `int`.
    - Added `Queue` property, initialized to `null!` for non-nullable reference type.

--- subway_project.Server/Models/Order.cs
+++ subway_project.Server/Models/Order.cs
@@ -5,10 +5,14 @@
         public int Id { get; set; }
         public bool TakeAway { get; set; }
         public decimal TotalPrice { get; set; }
-        public List<Product> Products { get; set; } = new();
+        public List<Product> Products { get; set; } = [];
+        public List<Sub> Subs { get; set; } = [];
+        public List<Topping> Toppings { get; set; } = [];
         public DateTime OrderReceived { get; set; } = DateTime.Now;
         public DateTime? OrderInProgress { get; set; }
         public DateTime? OrderCompleted { get; set; }
         public DateTime? OrderCollected { get; set; }
+        public int QueueId { get; set; }
+        public Queue Queue { get; set; } = null!;
     }
 }
