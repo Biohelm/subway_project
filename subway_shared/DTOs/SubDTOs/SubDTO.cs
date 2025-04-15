433a785a5d5fe01d686edab916d6abb8cae8c35a
RusandeKodare <o.p.j@hotmail.com>
2025-04-15 13:11:38 +0200

    Add SubDTO class with Toppings property
    - Introduced the SubDTO class in the subway_shared.DTOs.SubDTOs namespace.
    - Added a property Toppings of type List<ToppingDTO>, initialized with an empty list.
    - Included a using directive for ToppingDTOs to facilitate this addition.

---
+++ subway_shared/DTOs/SubDTOs/SubDTO.cs
@@ -0,0 +1,9 @@
+﻿using subway_shared.DTOs.ToppingDTOs;
+
+namespace subway_shared.DTOs.SubDTOs
+{
+    public class SubDTO
+    {
+        public List<ToppingDTO> Toppings { get; set; } = [];
+    }
+}
