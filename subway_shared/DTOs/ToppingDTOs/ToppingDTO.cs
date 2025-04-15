b7d39a580201662444642b8e22980ceababf9225
RusandeKodare <o.p.j@hotmail.com>
2025-04-15 13:12:04 +0200

    Add ToppingDTO class with new using directives
    - This commit introduces the `ToppingDTO` class in the `subway_shared.DTOs.ToppingDTOs` namespace.

---
+++ subway_shared/DTOs/ToppingDTOs/ToppingDTO.cs
@@ -0,0 +1,10 @@
+﻿using subway_shared.DTOs.SubCategoryDTOs;
+using subway_shared.DTOs.OrderDTOs;
+
+namespace subway_shared.DTOs.ToppingDTOs
+{
+    public class ToppingDTO
+    {
+        public string Name { get; set; } = null!;
+    }
+}
